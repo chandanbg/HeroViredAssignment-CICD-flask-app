01453f653575d52f7c3e267c8a4dfeb82e45f951
ChandanBG <38852803+chandanbg@users.noreply.github.com>
2026-04-18 11:08:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Auto trigger working 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
